e9a1126eb6675969e0bb5f438bedaa25a3b172a3
Mahmoud Ahmed <45967439+MAhmoud2527@users.noreply.github.com>
2020-07-19 01:32:01 +0200

    back to top

--- home/js/main.js
+++ home/js/main.js
@@ -259,4 +259,11 @@
 		$(this).addClass('active');
 	});
 	
+	// Animate the scroll to top
+	$('.go-top').click(function(event) {
+		event.preventDefault();
+		
+		$('html, body').animate({scrollTop: 0}, 800);
+	})
+
 })(window.jQuery); 
