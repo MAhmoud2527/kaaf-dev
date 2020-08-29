5ab524ef9983a8d48b29f862fe368c255b30bb68
Mahmoud Ahmed <45967439+MAhmoud2527@users.noreply.github.com>
2020-08-29 13:29:39 +0200

    upload file

---
+++ js/data.js
@@ -0,0 +1,177 @@
+
+var dataSrc = [
+	{
+        "text": "كورسات", 
+        "iconCls": "fa fa-folder"
+    },
+    {
+        "text": "فيزياء", 
+        "iconCls": "fa fa-folder"
+    },
+    {
+        "text": "علوم الحاسب",
+        "iconCls": "fa fa-folder",
+        "items": [
+            {
+                "text": "قدرات عامة",
+                "iconCls": "fa fa-folder"
+            }, {
+                "text": "فيزياء", 
+                "iconCls": "fa fa-folder"
+            }, {
+                "text": "فيزياء", 
+                "iconCls": "fa fa-folder"
+            },
+        ]
+    },{
+        "text": "كورسات", 
+        "iconCls": "fa fa-folder"
+    },
+    {
+        "text": "فيزياء", 
+        "iconCls": "fa fa-folder"
+    },
+    {
+        "text": "علوم الحاسب",
+        "iconCls": "fa fa-folder",
+        "items": [
+            {
+                "text": "قدرات عامة",
+                "iconCls": "fa fa-folder"
+            }
+        ]
+    },{
+        "text": "كورسات", 
+        "iconCls": "fa fa-folder"
+    },
+    {
+        "text": "فيزياء", 
+        "iconCls": "fa fa-folder"
+    },
+    {
+        "text": "علوم الحاسب",
+        "iconCls": "fa fa-folder",
+        "items": [
+            {
+                "text": "قدرات عامة",
+                "iconCls": "fa fa-folder"
+            }
+        ]
+    },
+]
+
+var folderData = 
+[
+    {
+        "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "علوم"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "حاسب الى"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "مادة اخرى"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "اختبارات"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "اخرى"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "تحصيل"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "قدرات"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "علوم "
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "رياضيات"
+    },
+    {
+        "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/vedio.png\">",
+        "text": "فيديو"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "فيزياء الكم"
+    }, 
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "لغة عربية"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "مزيكا"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "لغة دينية"
+    },{
+        "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "علوم"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "حاسب الى"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "مادة اخرى"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "اختبارات"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "اخرى"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "تحصيل"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "قدرات"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "علوم "
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "رياضيات"
+    },
+    {
+        "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/vedio.png\">",
+        "text": "فيديو"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "فيزياء الكم"
+    }, 
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "لغة عربية"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "مزيكا"
+    },
+    {
+       "icon": "<img class=\"img-responsive\" style=\"width:50px\" src=\"images/folder.png\">", 
+        "text": "لغة دينية"
+    }
+];
+
